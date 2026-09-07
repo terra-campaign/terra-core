325fdf9524777b42590d0b872777f5399a5296a7
MANUEL STAZ <staz01@hotmail.com>
2026-09-07 08:01:13 -0700

    Add getMissionEvidenceTotal export from mission-delegation

--- functions/index.js
+++ functions/index.js
@@ -4380,3 +4380,4 @@ exports.getMissionBranchProgress =
   require("./mission-delegation.cjs").getMissionBranchProgress;
 
 
+exports.getMissionEvidenceTotal = require("./mission-delegation.cjs").getMissionEvidenceTotal;
